69893553b8122cb5bcb2463d2c0ef1df0141dabe
padmajamarupaka <padmaja.marupaka@gmail.com>
2019-02-11 15:27:35 +0100

    intial commit

---
+++ app/src/main/java/com/example/germany/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.germany
+
+import android.content.Intent
+
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
+
+class MainActivity<view> : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
